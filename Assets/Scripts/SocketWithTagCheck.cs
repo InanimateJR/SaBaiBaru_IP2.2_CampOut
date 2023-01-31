edbe0632b8591595f132ec3264851bb4a46957f5
Ng shijun Jess <S10222791@connect.np.edu.sg>
2023-01-31 19:56:19 +0800

    Changing socket script
    Also removing testing the fish and stick interaction

--- Assets/Scripts/SocketWithTagCheck.cs
+++ Assets/Scripts/SocketWithTagCheck.cs
@@ -1,8 +1,10 @@
+using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+
 
 public class SocketWithTagCheck : XRSocketInteractor
 {
-    //Make sue there there's nothing in the string
+    /*
     public string targetTag = string.Empty;
 
     //When hover,if the object hovering has the target tag, interactor can hover it
@@ -21,5 +23,42 @@ public class SocketWithTagCheck : XRSocketInteractor
     private bool MatchUsingTag(XRBaseInteractable interactable)
     {
         return interactable.CompareTag(targetTag);
+    }
+    */
+
+    // XRSnapInteractor is an XRSocketInteractor that filters socketable items by tag
+    [Header("Filter PARAMS")]
+    [SerializeField] protected bool doFilterByTag = true;
+    [SerializeField] protected string[] filterTags;
+
+
+    public override bool CanHover(IXRHoverInteractable interactable)
+    {
+        if (doFilterByTag && interactable is MonoBehaviour gameObject)
+        {
+            foreach (string filterTag in filterTags)
+            {
+                if (!gameObject.CompareTag(filterTag))
+                {
+                    return false;
+                }
+            }
+        }
+        return base.CanHover(interactable);
+    }
+
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        if (doFilterByTag && interactable is MonoBehaviour gameObject)
+        {
+            foreach (string filterTag in filterTags)
+            {
+                if (!gameObject.CompareTag(filterTag))
+                {
+                    return false;
+                }
+            }
+        }
+        return base.CanSelect(interactable);
     }
 }
