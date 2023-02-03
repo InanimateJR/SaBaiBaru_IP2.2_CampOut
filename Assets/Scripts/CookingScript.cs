918c1f78e1d61acd9a90b9b34b4d351ccba6c6dc
Ng shijun Jess <S10222791@connect.np.edu.sg>
2023-02-03 23:31:26 +0800

    updating cooking script

--- Assets/Scripts/CookingScript.cs
+++ Assets/Scripts/CookingScript.cs
@@ -6,38 +6,36 @@ using UnityEngine;
 
 public class CookingScript : MonoBehaviour
 {
-    public bool foodSnapped;
 
     public GameObject fishToSpawn;
 
     public GameObject rawFish;
 
-    private void Start()
-    {
-        rawFish = GameObject.FindWithTag("Fish");
-    }
 
-    
-    public void SnappingGround()
-    {
-        var fire = GameObject.FindWithTag("fire");
-
-        if (fire != null)
-        {
-            Debug.Log("Fire is present");
-        }
-    }
-
-    void SnapFood()
-    {
-        foodSnapped = true;
-    }
 
     public void SpawnCookedFish()
     {
         GameObject cookedFish = Instantiate(fishToSpawn, rawFish.transform.position, rawFish.transform.rotation);
         rawFish.SetActive(false);
+        Debug.Log("Fish has been cooked!");
+
+    }
+
+    private void OnTriggerEnter(Collider objectNearFire)
+    {
+        if (objectNearFire.tag == "Fish")
+        {
+            Debug.Log("Fish is near fire");
+            rawFish = objectNearFire.gameObject;
+            StartCoroutine(CookingFish());
+        }
         
     }
 
+    IEnumerator CookingFish()
+    {
+        yield return new WaitForSeconds(5);
+        SpawnCookedFish();
+        
+    }
 }
