61e503ab7a82e1a83a927497cfcfe5704e9251ba
Ng shijun Jess <S10222791@connect.np.edu.sg>
2023-02-06 18:49:39 +0800

    Adding Poison effect

--- Assets/Scripts/EatingScript.cs
+++ Assets/Scripts/EatingScript.cs
@@ -5,25 +5,67 @@ using UnityEngine;
 public class EatingScript : MonoBehaviour
 {
 
+    public GameObject poisonEffect1;
+    public GameObject poisonEffect2;
+    public GameObject poisonEffect3;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Cooked Edible Mushroom")
         {
             
             Destroy(other.gameObject);
-            Debug.Log("Food as been eaten");
+            Debug.Log("Edible Mushroom has been eaten");
         }
         else if (other.gameObject.tag == "Cooked Fish")
         {
 
             Destroy(other.gameObject);
-            Debug.Log("Poison as been eaten");
+            Debug.Log("Fish has been eaten");
         }
         else if (other.gameObject.tag == "Cooked Poison Mushroom")
         {
-
             Destroy(other.gameObject);
-            Debug.Log("Poison as been eaten");
+            int effect = Random.Range(1, 4);
+            if (effect == 1)
+            {
+                poisonEffect1.SetActive(true);
+                StartCoroutine(PoisonEffect());
+            }
+            else if (effect == 2)
+            {
+                poisonEffect2.SetActive(true);
+                StartCoroutine(PoisonEffect());
+            }
+            else if (effect == 3)
+            {
+                poisonEffect1.SetActive(true);
+                StartCoroutine(PoisonEffect());
+            }
+            Debug.Log("Poison has been eaten");
         }
+
+        
     }
+    public IEnumerator PoisonEffect()
+    {
+        int effectTime = Random.Range(20, 31);
+        yield return new WaitForSeconds(effectTime);
+
+        if (poisonEffect1.activeSelf)
+        {
+            poisonEffect1.SetActive(false);
+        }
+        else if (poisonEffect2.activeSelf)
+        {
+            poisonEffect2.SetActive(false);
+        }
+        else if (poisonEffect3.activeSelf)
+        {
+            poisonEffect1.SetActive(false);
+        }
+        Debug.Log("Poison Effect is now gone");
+    }
+
 }
