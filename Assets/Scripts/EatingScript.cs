33bf1a700ed329a29b919778622d9ce03c75a418
Ng shijun Jess <S10222791@connect.np.edu.sg>
2023-02-06 17:14:55 +0800

    Update EatingScript.cs

--- Assets/Scripts/EatingScript.cs
+++ Assets/Scripts/EatingScript.cs
@@ -7,11 +7,23 @@ public class EatingScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Cooked Edible Mushroom" || other.gameObject.tag == "Cooked Fish" || other.gameObject.tag == "Cooked Poison Mushroom")
+        if(other.gameObject.tag == "Cooked Edible Mushroom")
         {
             
             Destroy(other.gameObject);
             Debug.Log("Food as been eaten");
+        }
+        else if (other.gameObject.tag == "Cooked Fish")
+        {
+
+            Destroy(other.gameObject);
+            Debug.Log("Poison as been eaten");
+        }
+        else if (other.gameObject.tag == "Cooked Poison Mushroom")
+        {
+
+            Destroy(other.gameObject);
+            Debug.Log("Poison as been eaten");
         }
     }
 }
