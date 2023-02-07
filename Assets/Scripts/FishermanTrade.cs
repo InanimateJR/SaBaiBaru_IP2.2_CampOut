d94c423f7b3d118cc295fa325ea6cae9b421e563
radelle <s10219371@connect.np.edu.sg>
2023-02-07 16:45:29 +0800

    fixed fishrod and firewood trading

--- Assets/Scripts/FishermanTrade.cs
+++ Assets/Scripts/FishermanTrade.cs
@@ -11,6 +11,9 @@ public class FishermanTrade : MonoBehaviour
     private int numberOfMushrooms;
     public GameObject fishRod;
     public GameObject arrow;
+    public List<GameObject> mushroomInBucket;
+    public Image start;
+    public Image end;
 
     // Start is called before the first frame update
     void Start()
@@ -27,24 +30,40 @@ public class FishermanTrade : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-       if(other.tag == "Mushroom")
+        if (other.tag == "Mushroom")
         {
-            Debug.Log("Mushroom in bucket");
-            numberOfMushrooms++;
-        }
-
-       if(numberOfMushrooms == 3)
-        {
-            Debug.Log("Mushroom quota fulfilled");
-            giveMushroom.interactable = true;
-
-            if(other.gameObject.tag == "Mushroom")
+            if (!mushroomInBucket.Contains(other.gameObject))
             {
-                Destroy(other.gameObject);
+                Debug.Log("Fish in bucket");
+                mushroomInBucket.Add(other.gameObject);
+                numberOfMushrooms++;
+                Debug.Log(numberOfMushrooms + " " + mushroomInBucket.Count);
+
+                if (numberOfMushrooms == 3)
+                {
+                    Debug.Log("Fish quota fulfilled");
+                    giveMushroom.interactable = true;
+
+                    for (int i = 2; i >= 0; --i)
+                    {
+                        Debug.Log("fish destroyed " + i);
+
+                        Destroy(mushroomInBucket[i]);
+                        mushroomInBucket.Remove(mushroomInBucket[i]);
+                    }
+                    Button btn = giveMushroom.GetComponent<Button>();
+                    btn.onClick.AddListener(TaskOnClick);
+                }
             }
 
+        }
+
+        void TaskOnClick()
+        {
+         
             fishRod.GetComponent<XRGrabInteractable>().enabled = true;
             fishRod.GetComponent<Rigidbody>().isKinematic = false;
+
             arrow.SetActive(true);
             rentRod.interactable = false;
         }
