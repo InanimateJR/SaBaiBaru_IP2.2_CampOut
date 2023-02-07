9b96cafdc8374016c43ed47c96cd291e4888edd5
clar16 <s10222932@connect.np.edu.sg>
2023-02-07 16:13:21 +0800

    added animation clip for 3 hits only first peg

--- Assets/Scripts/AnimationTrigger.cs
+++ Assets/Scripts/AnimationTrigger.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AnimationTrigger : MonoBehaviour
 {
     public AnimationClip animationClip;
+    public AnimationClip animationClip1;
+    public AnimationClip animationClip2;
     private int hits = 0;
     private Animation animation;
 
