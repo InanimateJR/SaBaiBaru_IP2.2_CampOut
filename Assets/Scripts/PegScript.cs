05a622a90c389dd315946a9c2f04b6476892a9e6
Guo Ji Xuan <S10222055@connect.np.edu.sg>
2023-02-11 16:33:14 +0800

    Tent Pegs working

---
+++ Assets/Scripts/PegScript.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PegScript : MonoBehaviour
+{
+    int timesHit = 0;
+    public bool pegSnapped;
+    public GameObject pegSocket1;
+    public GameObject pegSocket2;
+    public GameObject pegSocket3;
+    public GameObject pegSocket4;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Mallet")
+        {
+            Debug.Log("Hit");
+            if (timesHit < 3 && pegSnapped)
+            {
+                this.gameObject.transform.position += new Vector3(0, -0.03f, 0);
+                timesHit++;
+            }
+        }
+    }
+
+    public void SetPegSnapped()
+    {
+        pegSnapped = true;
+        if (this.gameObject.name == "Peg 1")
+        {
+            StartCoroutine("DestroySocket1");
+        }
+
+        if (this.gameObject.name == "Peg 2")
+        {
+            StartCoroutine("DestroySocket2");
+        }
+
+        if (this.gameObject.name == "Peg 3")
+        {
+            StartCoroutine("DestroySocket3");
+        }
+
+        if (this.gameObject.name == "Peg 4")
+        {
+            StartCoroutine("DestroySocket4");
+        }
+    }
+
+    IEnumerator DestroySocket1()
+    {
+        XRGrabInteractable interactableComponent = this.gameObject.GetComponent<XRGrabInteractable>();
+        Rigidbody rigidBodyComponent = this.gameObject.GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(1f);
+        Destroy(interactableComponent);
+        rigidBodyComponent.isKinematic = true;
+        rigidBodyComponent.useGravity = false;
+        Destroy(pegSocket1);
+    }
+
+    IEnumerator DestroySocket2()
+    {
+        XRGrabInteractable interactableComponent = this.gameObject.GetComponent<XRGrabInteractable>();
+        Rigidbody rigidBodyComponent = this.gameObject.GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(1f);
+        Destroy(interactableComponent);
+        rigidBodyComponent.isKinematic = true;
+        rigidBodyComponent.useGravity = false;
+        Destroy(pegSocket2);
+    }
+
+    IEnumerator DestroySocket3()
+    {
+        XRGrabInteractable interactableComponent = this.gameObject.GetComponent<XRGrabInteractable>();
+        Rigidbody rigidBodyComponent = this.gameObject.GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(1f);
+        Destroy(interactableComponent);
+        rigidBodyComponent.isKinematic = true;
+        rigidBodyComponent.useGravity = false;
+        Destroy(pegSocket3);
+    }
+    IEnumerator DestroySocket4()
+    {
+        XRGrabInteractable interactableComponent = this.gameObject.GetComponent<XRGrabInteractable>();
+        Rigidbody rigidBodyComponent = this.gameObject.GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(1f);
+        Destroy(interactableComponent);
+        rigidBodyComponent.isKinematic = true;
+        rigidBodyComponent.useGravity = false;
+        Destroy(pegSocket4);
+    }
+}
