7de63f877be5577d02fee10674b60b51bd7e6366
radelle <s10219371@connect.np.edu.sg>
2023-02-15 23:40:20 +0800

    loading scene done

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    public GameObject XROrigin;
+    public GameObject xrOrigin;
+    private Transform PlayerTransform;
+    public Transform TeleportGoal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerTransform = xrOrigin.transform;
     }
 
     // Update is called once per frame
@@ -19,6 +23,19 @@ public class LoadScene : MonoBehaviour
     
     public void MoveToCube()
     {
+        PlayerTransform.position = TeleportGoal.position;
+        StartCoroutine(LoadSceneAsync());
+    }
 
+    IEnumerator LoadSceneAsync()
+    {
+        //The Application loads the Scene in the background as the current Scene runs.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
